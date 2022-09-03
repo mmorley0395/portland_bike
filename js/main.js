0e4a0d2f2c1156782be330dc9e50bfe842c29cc4
mmorley <mmorley0395@gmail.com>
2022-09-02 21:11:01 -0700

    buttons with visibility working

--- js/main.js
+++ js/main.js
@@ -4,44 +4,111 @@ import { frame, makeAnimation, startTime, duration } from "./animate.js";
 export let map = makemap();
 
 map.on("load", () => {
-  map.addSource("day0", {
+  map.addSource("day1", {
     type: "geojson",
-    data: "https://mmorley0395.github.io/portland_bike/routes/dayzero.json",
+    data: "https://mmorley0395.github.io/portland_bike/routes/dayone.json",
     lineMetrics: true,
   });
   map.addLayer({
-    id: "route-line",
+    id: "day1",
     type: "line",
-    source: "day0",
+    source: "day1",
     paint: {
       "line-color": "rgba(101,101,101,101)",
       "line-width": 8,
       "line-opacity": 0.7,
     },
   });
-  makeAnimation();
-  // map.addSource("day1", {
-  //   type: "geojson",
-  //   data: "https://mmorley0395.github.io/portland_bike/routes/dayone.json",
-  //   lineMetrics: true,
-  // });
-  // map.addSource("day2", {
-  //   type: "geojson",
-  //   data: "https://mmorley0395.github.io/portland_bike/routes/daytwo.json",
-  //   lineMetrics: true,
-  // });
-  // map.addSource("day3", {
-  //   type: "geojson",
-  //   data: "https://mmorley0395.github.io/portland_bike/routes/daythree.json",
-  //   lineMetrics: true,
-  // });
+
+  map.addSource("day2", {
+    type: "geojson",
+    data: "https://mmorley0395.github.io/portland_bike/routes/daytwo.json",
+    lineMetrics: true,
+  });
+  map.addLayer({
+    id: "day2",
+    type: "line",
+    source: "day2",
+    paint: {
+      "line-color": "rgba(101,101,101,101)",
+      "line-width": 8,
+      "line-opacity": 0.7,
+    },
+  });
+
+  map.addSource("day3", {
+    type: "geojson",
+    data: "https://mmorley0395.github.io/portland_bike/routes/daythree.json",
+    lineMetrics: true,
+  });
+  map.addLayer({
+    id: "day3",
+    type: "line",
+    source: "day3",
+    paint: {
+      "line-color": "rgba(101,101,101,101)",
+      "line-width": 8,
+      "line-opacity": 0.7,
+    },
+  });
+
   // map.addSource("day4", {
   //   type: "geojson",
   //   data: "https://mmorley0395.github.io/portland_bike/routes/dayfour.json",
   //   lineMetrics: true,
   // });
+  // map.addLayer({
+  //   id: "day4",
+  //   type: "line",
+  //   source: "day4",
+  //   paint: {
+  //     "line-color": "rgba(101,101,101,101)",
+  //     "line-width": 8,
+  //     "line-opacity": 0.7,
+  //   },
+  // });
 });
+map.on("idle", () => {
+  // If these two layers were not added to the map, abort
+  if (!map.getLayer("day1") || !map.getLayer("day2") || !map.getLayer("day3")) {
+    return;
+  }
+  // Enumerate ids of the layers.
+  const toggleableLayerIds = ["day1", "day2", "day3"];
+  // Set up the corresponding toggle button for each layer.
+  for (const id of toggleableLayerIds) {
+    // Skip layers that already have a button set up.
+    if (document.getElementById(id)) {
+      continue;
+    }
+    // Create a link.
+    const link = document.createElement("a");
+    link.id = id;
+    link.href = "#";
+    link.textContent = id;
+    link.className = "active";
+    // Show or hide layer when the toggle is clicked.
+    link.onclick = function (e) {
+      const clickedLayer = this.textContent;
+      e.preventDefault();
+      e.stopPropagation();
 
+      const visibility = map.getLayoutProperty(clickedLayer, "visibility");
+
+      // Toggle layer visibility by changing the layout object's visibility property.
+      if (visibility === "visible") {
+        map.setLayoutProperty(clickedLayer, "visibility", "none");
+        this.className = "";
+      } else {
+        this.className = "active";
+        map.setLayoutProperty(clickedLayer, "visibility", "visible");
+      }
+    };
+
+    const layers = document.getElementById("menu");
+    layers.appendChild(link);
+  }
+});
 // class ControlButton {
 //   onAdd(map) {
 //     this.map = map;
